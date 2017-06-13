7703fb5f65f44824493c02d09e880d32c7023c79
Alex Hill <big_al_hill@comcast.net>
2017-06-12 20:58:42 -0600

    REACT basic afternoon project complete

---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+
+
+class Sum extends Component {
+
+    render () {
+        return (
+            <p> Sum </p>
+
+
+        )
+    }
+}
+
+export default Sum;
